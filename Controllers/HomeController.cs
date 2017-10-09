f114b7e5643fb72990692a0cecc577dcfe394d0b
Adam Titus and Kyle Ebersole <adam@>
2017-10-09 14:35:57 -0700

    Add madlibs.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,19 +5,54 @@ namespace FriendLetter.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/")]
+    [Route("/hello")]
     public ActionResult Hello()
     {
       LetterVariable myLetterVariable = new LetterVariable();
       myLetterVariable.SetRecipient("Adam");
       myLetterVariable.SetVacation("Thailand");
       myLetterVariable.SetSouvenir("Coconut Bikini");
+      myLetterVariable.SetSender("Snuffles");
       return View(myLetterVariable);
     }
     [Route("/favorite_photos")]
     public ActionResult FavoritePhotos()
     {
       return View();
+    }
+    [Route("/form")]
+    public ActionResult Form()
+    {
+      return View();
+    }
+    [Route("/greeting_card")]
+    public ActionResult GreetingCard()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.SetRecipient(Request.Query["recipient"]);
+      myLetterVariable.SetSender(Request.Query["sender"]);
+
+      return View("Hello", myLetterVariable);
+    }
+
+    [Route("/madlibs")]
+    public ActionResult Madlibs()
+    {
+      return View();
+    }
+
+    [Route("/story_time")]
+    public ActionResult StoryTime()
+    {
+      Madlibs myMadLibs = new Madlibs();
+      myMadLibs.SetName(Request.Query["name"]);
+      myMadLibs.SetAnimal(Request.Query["animal"]);
+      myMadLibs.SetFood(Request.Query["food"]);
+      myMadLibs.SetAdjective(Request.Query["adjective"]);
+      myMadLibs.SetVerb(Request.Query["verb"]);
+      myMadLibs.SetNoun(Request.Query["noun"]);
+
+      return View(myMadLibs);
     }
   }
 }
--- Models/LetterVariable.cs
+++ Models/LetterVariable.cs
@@ -5,6 +5,7 @@ namespace FriendLetter.Models
     private string _recipient;
     private string _vacation;
     private string _souvenir;
+    private string _sender;
 
     public string GetRecipient()
     {
@@ -34,6 +35,80 @@ namespace FriendLetter.Models
     public void SetSouvenir(string newSouvenir)
     {
       _souvenir = newSouvenir;
+    }
+
+    public string GetSender()
+    {
+      return _sender;
+    }
+
+    public void SetSender(string newSender)
+    {
+      _sender = newSender;
+    }
+  }
+  public class Madlibs
+  {
+    private string _name;
+    private string _animal;
+    private string _food;
+    private string _adjective;
+    private string _verb;
+    private string _noun;
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public string GetAnimal()
+    {
+      return _animal;
+    }
+
+    public void SetAnimal(string newAnimal)
+    {
+      _animal = newAnimal;
+    }
+    public string GetFood()
+    {
+      return _food;
+    }
+
+    public void SetFood(string newFood)
+    {
+      _food = newFood;
+    }
+    public string GetAdjective()
+    {
+      return _adjective;
+    }
+
+    public void SetAdjective(string newAdjective)
+    {
+      _adjective = newAdjective;
+    }
+    public string GetVerb()
+    {
+      return _verb;
+    }
+
+    public void SetVerb(string newVerb)
+    {
+      _verb = newVerb;
+    }
+    public string GetNoun()
+    {
+      return _noun;
+    }
+
+    public void SetNoun(string newNoun)
+    {
+      _noun = newNoun;
     }
   }
 }
